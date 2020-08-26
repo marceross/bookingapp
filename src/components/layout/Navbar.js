6c0c17f793d4c251aa792751902943575aa16631
caleblipan <caleb.lipan99@gmail.com>
2020-08-26 15:53:07 +0000

    Completed the navigation bar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,15 +6,25 @@ import logo from './images/logo.png';
 class Navbar extends Component {
     render() {
         return (
-            <nav>
-                <ul style={{listStyleType: 'none'}}>
-                    <li className="navbar-logo">
-                        <img src={logo} alt="The Cumpa Logo"/>
-                    </li>
-                </ul>
-                <div id="search-bar">
-                </div>
-            </nav>
+            <div className="col-12 navbar">
+        		<div className="center-col-12 row">
+        			<div className="col-5">
+                		<ul>
+                    		<li className="navbar-logo">
+                    	    	<img src={logo} alt="The Cumpa Logo"/>
+                    		</li>
+                		</ul>
+					</div>
+					<div className="col-7">
+                		<div id="search-bar">
+                			<form>
+                				<input type="search" placeholder="Fit / Gym"/>
+                                <i className="fa fa-search"></i>
+                			</form>
+                		</div>
+					</div>
+				</div>
+            </div>
         );
     }
 }
